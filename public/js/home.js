6d1dba0f18c278fa71d7d8ace2b8ebf210d841ab
danjohnvelasco <54620209+danjohnvelasco@users.noreply.github.com>
2021-01-11 12:28:01 +0800

    added forms and working query

--- public/js/home.js
+++ public/js/home.js
@@ -1,57 +1,332 @@
 $(document).ready(() => {
+  console.log("home.js imported")
   // Add click event handler
-  $('#btn1').click(function(event) {
-    event.preventDefault(); // prevent refresh
-    readHandler();
+  $('#select-form-id').change(function() {
+    console.log('render form' + $(this).val());
+    getForm($(this).val());
   });
+
+  $('#query-btn').click(function(event) {
+    event.preventDefault(); // prevent refresh
+    var data = getFormInput();
+    queryHandler(data);
+  });
+
+  
 });
 
-function readHandler() {
+function getFormInput() {
+  var obj = {};
+  $("form#form-div :input").each(function(){
+    var key = $(this).attr("class"); 
+    var val = $(this).val(); 
+    obj[key] = val
+  });
+
+  return obj
+}
+
+function getForm(formID) {
+  // EMPTY DIV
+  $('#form-div').empty();
+
+  // which form
+  switch(formID) {
+    case "query1":
+      query1_form();
+      break;
+    case "query2":
+      query2_form();
+      break;
+    case "query3":
+      query3_form();
+      break;
+    case "query4":
+      query4_form();
+      break;
+    case "query5":
+      query5_form();
+      break;
+    case "query6":
+      query6_form();
+      break;
+    case "query7":
+      query7_form();
+      break;
+    default:
+      console.log("no form to render");
+  }
+
+}
+
+function queryHandler(data_obj) {
+  var queryURL = $('#select-form-id').val();
   $.ajax({
-    url: '/api/read',
+    url: '/api/' + queryURL,
     method: 'GET',
-    //data: {user: userId},
+    data: {data: data_obj},
     success: (data, status) => {
-      // append images to gallery    
+      console.table(data);
+      /*
       data.forEach((item, index) => {
-        addToGallery(item, gallery);
-      });
-      // set fancybox instance options
-      $('[data-fancybox]').fancybox({
-        toolbar: true,
-        smallBtn: false,
-        buttons: ["close"],
-        infobar: false,
-        iframe: {
-          preload: false
-        },
-        arrows: false,
-        touch: false
-      })
+      });*/
     },
     error: () => {
-      console.log('error gallery.js');
+      console.log('error requesting from API...');
     }
   });
 }
 // Helper functions
 
-/*
-//this function adds an image to the gallery
-function addToGallery(item, parentDiv) {
+function query1_form() {
   // DOM Creation
-  var linkDiv = document.createElement('div');
-  var link = document.createElement('a');
-  var img = document.createElement('img');
+  var formGroup1 = document.createElement('div');
+  var formGroup2 = document.createElement('div');
+  var formGroup3 = document.createElement('div');
+  var genre = document.createElement('input');
+  var startYear = document.createElement('input');
+  var endYear = document.createElement('input');
+
   // adding attributes
-  $(img).attr('src', `/post/image/${item.img}`);
-  $(link).attr('data-fancybox', '');    
-  $(link).attr('data-type', 'ajax');
-  $(link).attr('data-src', `/post/${item._id}`);
-  $(link).attr('href', 'javascript:;');
+  $(formGroup1).attr('class', 'form-group');
+  $(formGroup2).attr('class', 'form-group');
+  $(formGroup3).attr('class', 'form-group');
+  $(genre).attr('type', 'text');
+  $(startYear).attr('type', 'text');
+  $(endYear).attr('type', 'text');
+  $(genre).attr('class', 'genre');
+  $(startYear).attr('class', 'startYear');
+  $(endYear).attr('class', 'endYear');
+  $(genre).attr('placeholder', 'Genre');
+  $(startYear).attr('placeholder', 'Start Year');
+  $(endYear).attr('placeholder', 'Max Year');
+
+
   // appending
-  $(link).append(img);
-  $(linkDiv).append(link);
-  $(parentDiv).append(linkDiv);
+  $(formGroup1).append(genre);
+  $(formGroup2).append(startYear);
+  $(formGroup3).append(endYear);
+
+  $('#form-div').append(formGroup1);
+  $('#form-div').append(formGroup2);
+  $('#form-div').append(formGroup3);
 }
-*/
+
+function query2_form() {
+  // DOM Creation
+  var formGroup1 = document.createElement('div');
+  var formGroup2 = document.createElement('div');
+  var formGroup3 = document.createElement('div');
+  var formGroup4 = document.createElement('div');
+  var country = document.createElement('input');
+  var language = document.createElement('input');
+  var startYear = document.createElement('input');
+  var endYear = document.createElement('input');
+
+  // adding attributes
+  $(formGroup1).attr('class', 'form-group');
+  $(formGroup2).attr('class', 'form-group');
+  $(formGroup3).attr('class', 'form-group');
+  $(formGroup4).attr('class', 'form-group');
+
+  $(country).attr('type', 'text');
+  $(language).attr('type', 'text');
+  $(startYear).attr('type', 'text');
+  $(endYear).attr('type', 'text');
+
+  $(country).attr('class', 'country');
+  $(language).attr('class', 'language');
+  $(startYear).attr('class', 'startYear');
+  $(endYear).attr('class', 'endYear');
+
+  $(country).attr('placeholder', 'Country');
+  $(language).attr('placeholder', 'Language');
+  $(startYear).attr('placeholder', 'Start Year');
+  $(endYear).attr('placeholder', 'Max Year');
+
+
+  // appending
+  $(formGroup1).append(country);
+  $(formGroup2).append(language);
+  $(formGroup3).append(startYear);
+  $(formGroup4).append(endYear);
+
+  $('#form-div').append(formGroup1);
+  $('#form-div').append(formGroup2);
+  $('#form-div').append(formGroup3);
+  $('#form-div').append(formGroup4);
+}
+
+function query3_form() {
+  // DOM Creation
+  var formGroup1 = document.createElement('div');
+  var n_leadingroles = document.createElement('input');
+
+  // adding attributes
+  $(formGroup1).attr('class', 'form-group');
+
+  $(n_leadingroles).attr('type', 'text');
+
+  $(n_leadingroles).attr('class', 'n_leadingroles');
+
+  $(n_leadingroles).attr('placeholder', 'Number of leading roles');
+
+  // appending
+  $(formGroup1).append(n_leadingroles);
+
+  $('#form-div').append(formGroup1);
+}
+
+function query4_form() {
+  // DOM Creation
+  var formGroup1 = document.createElement('div');
+  var formGroup2 = document.createElement('div');
+  var formGroup3 = document.createElement('div');
+  var year = document.createElement('input');
+  var min_avg_vote = document.createElement('input');
+  var max_avg_vote = document.createElement('input');
+
+  // adding attributes
+  $(formGroup1).attr('class', 'form-group');
+  $(formGroup2).attr('class', 'form-group');
+  $(formGroup3).attr('class', 'form-group');
+  $(year).attr('type', 'text');
+  $(min_avg_vote).attr('type', 'text');
+  $(max_avg_vote).attr('type', 'text');
+
+  $(year).attr('class', 'year');
+  $(min_avg_vote).attr('class', 'min_avg_vote');
+  $(max_avg_vote).attr('class', 'max_avg_vote');
+
+  $(year).attr('placeholder', 'Year');
+  $(min_avg_vote).attr('placeholder', 'Min. Avg Vote');
+  $(max_avg_vote).attr('placeholder', 'Max. Avg Vote');
+
+  // appending
+  $(formGroup1).append(year);
+  $(formGroup2).append(min_avg_vote);
+  $(formGroup3).append(max_avg_vote);
+
+  $('#form-div').append(formGroup1);
+  $('#form-div').append(formGroup2);
+  $('#form-div').append(formGroup3);
+}
+
+function query5_form() {
+  // DOM Creation
+  var formGroup1 = document.createElement('div');
+  var formGroup2 = document.createElement('div');
+  var formGroup3 = document.createElement('div');
+  var formGroup4 = document.createElement('div');
+  var country = document.createElement('input');
+  var genre = document.createElement('input');
+  var startYear = document.createElement('input');
+  var endYear = document.createElement('input');
+
+  // adding attributes
+  $(formGroup1).attr('class', 'form-group');
+  $(formGroup2).attr('class', 'form-group');
+  $(formGroup3).attr('class', 'form-group');
+  $(formGroup4).attr('class', 'form-group');
+
+  $(country).attr('type', 'text');
+  $(genre).attr('type', 'text');
+  $(startYear).attr('type', 'text');
+  $(endYear).attr('type', 'text');
+
+  $(country).attr('class', 'country');
+  $(genre).attr('class', 'genre');
+  $(startYear).attr('class', 'startYear');
+  $(endYear).attr('class', 'endYear');
+
+  $(country).attr('placeholder', 'Country');
+  $(genre).attr('placeholder', 'Genre');
+  $(startYear).attr('placeholder', 'Start Year');
+  $(endYear).attr('placeholder', 'Max Year');
+
+
+  // appending
+  $(formGroup1).append(genre);
+  $(formGroup2).append(country);
+  $(formGroup3).append(startYear);
+  $(formGroup4).append(endYear);
+
+  $('#form-div').append(formGroup1);
+  $('#form-div').append(formGroup2);
+  $('#form-div').append(formGroup3);
+  $('#form-div').append(formGroup4);
+}
+
+function query6_form() {
+  // DOM Creation
+  var formGroup1 = document.createElement('div');
+
+  var country = document.createElement('input');
+
+  // adding attributes
+  $(formGroup1).attr('class', 'form-group');
+  
+  $(country).attr('type', 'text');
+
+  $(country).attr('class', 'country');
+
+  $(country).attr('placeholder', 'Country');
+
+  // appending
+  $(formGroup1).append(country);
+
+  $('#form-div').append(formGroup1);
+}
+
+function query7_form() {
+  // DOM Creation
+  var formGroup1 = document.createElement('div');
+  var formGroup2 = document.createElement('div');
+  var formGroup3 = document.createElement('div');
+  var formGroup4 = document.createElement('div');
+  var formGroup5 = document.createElement('div');
+
+  var minAge = document.createElement('input');
+  var maxAge = document.createElement('input');
+  var country = document.createElement('input');
+  var minRating = document.createElement('input');
+  var maxRating = document.createElement('input');
+
+  // adding attributes
+  $(formGroup1).attr('class', 'form-group');
+  $(formGroup2).attr('class', 'form-group');
+  $(formGroup3).attr('class', 'form-group');
+  $(formGroup4).attr('class', 'form-group');
+  $(formGroup5).attr('class', 'form-group');
+
+  $(minAge).attr('type', 'text');
+  $(maxAge).attr('type', 'text');
+  $(country).attr('type', 'text');
+  $(minRating).attr('type', 'text');
+  $(maxRating).attr('type', 'text');
+  
+  $(minAge).attr('class', 'minAge');
+  $(maxAge).attr('class', 'maxAge');
+  $(country).attr('class', 'country');
+  $(minRating).attr('class', 'minRating');
+  $(maxRating).attr('class', 'maxRating');
+
+  $(minAge).attr('placeholder', 'Min. Age');
+  $(maxAge).attr('placeholder', 'Max. Age');
+  $(country).attr('placeholder', 'Country');
+  $(minRating).attr('placeholder', 'Min. Rating');
+  $(maxRating).attr('placeholder', 'Max. Rating');
+
+  // appending
+  $(formGroup1).append(country);
+  $(formGroup2).append(minAge);
+  $(formGroup3).append(maxAge);
+  $(formGroup4).append(minRating);
+  $(formGroup5).append(maxRating);
+
+  $('#form-div').append(formGroup1);
+  $('#form-div').append(formGroup2);
+  $('#form-div').append(formGroup3);
+  $('#form-div').append(formGroup4);
+  $('#form-div').append(formGroup5);
+}
--- routes/apiRouter.js
+++ routes/apiRouter.js
@@ -57,7 +57,10 @@ connect();
 router.get('/query1', (req, res) => {
     //“Movies that are released from [year] to [year] with a genre of [genre].”
     console.log("querying...");
-    conn.query('SELECT title, genre, year FROM movies WHERE genre LIKE ? AND year BETWEEN ? AND ? ORDER BY year', ['%Drama%', 1900,2000], 
+    console.log(req.query.data);
+    var data = req.query.data;
+    var genre = '%' + data.genre + '%';
+    conn.query('SELECT title, genre, year FROM movies WHERE genre LIKE ? AND year BETWEEN ? AND ? ORDER BY year LIMIT 2', [genre,parseInt(data.startYear),parseInt(data.endYear)], 
         function (err, results, fields) {
             if (err) throw err;
             else console.log('Selected ' + results.length + ' row(s).');
@@ -70,7 +73,11 @@ router.get('/query1', (req, res) => {
 router.get('/query2', (req, res) => {
     // "Films from [country] in [Language] language released in [year] to [year]."
     console.log("querying...");
-    conn.query('SELECT title, country, language, year FROM movies WHERE language LIKE ? AND country LIKE ? AND year BETWEEN ? AND ? ORDER BY year', ['%Filipino%', '%Philippines', 2000, 2020], 
+    console.log(req.query.data);
+    var data = req.query.data;
+    var country = '%' + data.country + '%';
+    var language = '%' + data.language + '%';
+    conn.query('SELECT title, country, language, year FROM movies WHERE language LIKE ? AND country LIKE ? AND year BETWEEN ? AND ? ORDER BY year LIMIT 2', [language,country,parseInt(data.startYear),parseInt(data.endYear)], 
         function (err, results, fields) {
             if (err) throw err;
             else console.log('Selected ' + results.length + ' row(s).');
@@ -85,7 +92,9 @@ router.get('/query2', (req, res) => {
 router.get('/query3', (req, res) => {
     //"Best Faces of a Country"
     console.log("querying...");
-    conn.query('SELECT DISTINCT name, COUNT(names.imdb_name_id) AS "No. of leading roles" FROM names JOIN title_principals ON names.imdb_name_id = title_principals.imdb_name_id WHERE title_principals.ordering = 1 AND (title_principals.category = "actress" OR title_principals.category = "actor") GROUP BY names.imdb_name_id HAVING COUNT(*) > ?',[5], 
+    console.log(req.query.data);
+    var data = req.query.data;
+    conn.query('SELECT DISTINCT name, COUNT(names.imdb_name_id) AS "No. of leading roles" FROM names JOIN title_principals ON names.imdb_name_id = title_principals.imdb_name_id WHERE title_principals.ordering = 1 AND (title_principals.category = "actress" OR title_principals.category = "actor") GROUP BY names.imdb_name_id HAVING COUNT(*) > ? ORDER BY COUNT(names.imdb_name_id) DESC',[parseInt(data.n_leadingroles)], 
         function (err, results, fields) {
             if (err) throw err;
             else console.log('Selected ' + results.length + ' row(s).');
@@ -99,7 +108,9 @@ router.get('/query3', (req, res) => {
 router.get('/query4', (req, res) => {
     //“Film Performances in a Year”
     console.log("querying...");
-    conn.query('SELECT DISTINCT m.title, r.weighted_average_vote AS rating FROM movies m CROSS JOIN ratings r ON m.imdb_title_id = r.imdb_title_id WHERE m.year = ? AND r.weighted_average_vote BETWEEN ? AND ? ORDER BY rating', [2010,2,3], 
+    console.log(req.query.data);
+    var data = req.query.data;
+    conn.query('SELECT DISTINCT m.title, r.weighted_average_vote AS rating FROM movies m CROSS JOIN ratings r ON m.imdb_title_id = r.imdb_title_id WHERE m.year = ? AND r.weighted_average_vote BETWEEN ? AND ? ORDER BY rating DESC', [parseInt(data.year),parseInt(data.min_avg_vote),parseInt(data.max_avg_vote)], 
         function (err, results, fields) {
             if (err) throw err;
             else console.log('Selected ' + results.length + ' row(s).');
@@ -113,7 +124,11 @@ router.get('/query4', (req, res) => {
 router.get('/query5', (req, res) => {
     //“Top 10 Most-casted actors/actresses of a Genre from a Year Interval in a Country”
     console.log("querying...");
-    conn.query('SELECT n.name AS "Actor/Actress", COUNT(n.name) AS "Number of Romance Films casted from 2010-2020 in USA", MAX(m.year) AS "Year of Last Romance Movie" FROM movies m INNER JOIN title_principals p ON m.imdb_title_id = p.imdb_title_id AND (p.category = "actor" OR p.category = "actress") INNER JOIN names n ON p.imdb_name_id = n.imdb_name_id WHERE m.year >= ? AND m.year <= ? AND m.country LIKE ? AND m.genre LIKE ? GROUP BY n.name ORDER BY COUNT(n.name) DESC, MAX(m.year) DESC, n.name ASC LIMIT 10', [2010, 2020, '%Philippines%', '%Romance%'], 
+    console.log(req.query.data);
+    var data = req.query.data;
+    var country = '%' + data.country + '%';
+    var genre = '%' + data.genre + '%';
+    conn.query('SELECT n.name AS "Actor/Actress", COUNT(n.name) AS "Number of Romance Films casted from 2010-2020 in USA", MAX(m.year) AS "Year of Last Romance Movie" FROM movies m INNER JOIN title_principals p ON m.imdb_title_id = p.imdb_title_id AND (p.category = "actor" OR p.category = "actress") INNER JOIN names n ON p.imdb_name_id = n.imdb_name_id WHERE m.year >= ? AND m.year <= ? AND m.country LIKE ? AND m.genre LIKE ? GROUP BY n.name ORDER BY COUNT(n.name) DESC, MAX(m.year) DESC, n.name ASC LIMIT 10', [parseInt(data.startYear), parseInt(data.endYear), country, genre], 
         function (err, results, fields) {
             if (err) throw err;
             else console.log('Selected ' + results.length + ' row(s).');
@@ -127,7 +142,10 @@ router.get('/query5', (req, res) => {
 router.get('/query6', (req, res) => {
     //“Notable Foreign-born Directors of a Country”
     console.log("querying...");
-    conn.query('SELECT n.name AS "Director", n.place_of_birth AS "Place of Birth", COUNT(n.name) AS "Number of Films Directed in the USA", MAX(m.year) AS "Year of Last Film" FROM movies m INNER JOIN title_principals p ON m.imdb_title_id = p.imdb_title_id AND p.category = "director" INNER JOIN names n ON p.imdb_name_id = n.imdb_name_id AND n.place_of_birth != "" AND n.place_of_birth NOT LIKE ? WHERE m.country LIKE ? GROUP BY n.name, n.place_of_birth ORDER BY COUNT(n.name) DESC, MAX(m.year) DESC, n.name ASC LIMIT 5', ['%USA%', '%USA%'],
+    console.log(req.query.data);
+    var data = req.query.data;
+    var country = '%' + data.country + '%';
+    conn.query('SELECT n.name AS "Director", n.place_of_birth AS "Place of Birth", COUNT(n.name) AS "Number of Films Directed in the USA", MAX(m.year) AS "Year of Last Film" FROM movies m INNER JOIN title_principals p ON m.imdb_title_id = p.imdb_title_id AND p.category = "director" INNER JOIN names n ON p.imdb_name_id = n.imdb_name_id AND n.place_of_birth != "" AND n.place_of_birth NOT LIKE ? WHERE m.country LIKE ? GROUP BY n.name, n.place_of_birth ORDER BY COUNT(n.name) DESC, MAX(m.year) DESC, n.name ASC LIMIT 5', [country, country],
         function (err, results, fields) {
             if (err) throw err;
             else console.log('Selected ' + results.length + ' row(s).');
@@ -142,7 +160,10 @@ router.get('/query6', (req, res) => {
 router.get('/query7', (req, res) => {
     //"[Actors/Actresses] in the age group of [min age] to [max age] who have casted on a movie from [country] with an average rating from [min rating] to [max rating]."
     console.log("querying...");
-    conn.query('SELECT n.name AS "Name", t.category AS "Gender", COUNT(t.imdb_name_id) AS "Number of Roles" FROM names n LEFT JOIN title_principals t ON n.imdb_name_id = t.imdb_name_id LEFT JOIN movies m ON t.imdb_title_id = m.imdb_title_id LEFT JOIN ratings r ON m.imdb_title_id = r.imdb_title_id WHERE (t.category = "actor" OR t.category = "actress") AND (YEAR(NOW() - YEAR(n.date_of_birth) >= ?) AND (YEAR(NOW() - YEAR(n.date_of_birth) <= ?) AND m.country = ? AND (r.weighted_average_vote >= ?) AND (r.weighted_average_vote <= ?) GROUP BY t.imdb_name_id ORDER BY n.name', [20, 29, '%Philippines%', 3,5],
+    console.log(req.query.data);
+    var data = req.query.data;
+    var country = '%' + data.country + '%';
+    conn.query('SELECT n.name AS "Name", t.category AS "Gender", COUNT(t.imdb_name_id) AS "Number of Roles" FROM names n LEFT JOIN title_principals t ON n.imdb_name_id = t.imdb_name_id LEFT JOIN movies m ON t.imdb_title_id = m.imdb_title_id LEFT JOIN ratings r ON m.imdb_title_id = r.imdb_title_id WHERE (t.category = "actor" OR t.category = "actress") AND (YEAR(NOW() - YEAR(n.date_of_birth) >= ?)) AND (YEAR(NOW() - YEAR(n.date_of_birth) <= ?)) AND m.country = ? AND (r.weighted_average_vote >= ?) AND (r.weighted_average_vote <= ?) GROUP BY t.imdb_name_id, t.category ORDER BY n.name', [parseInt(data.minAge), parseInt(data.maxAge), country, parseInt(data.minRating), parseInt(data.maxRating)],
         function (err, results, fields) {
             if (err) throw err;
             else console.log('Selected ' + results.length + ' row(s).');
