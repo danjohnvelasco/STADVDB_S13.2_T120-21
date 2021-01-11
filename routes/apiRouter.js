1827858a6df32e3f3b4b6a6f8d51c87645d71c55
danjohnvelasco <54620209+danjohnvelasco@users.noreply.github.com>
2021-01-11 16:10:58 +0800

    added error handling on API. Prevent crashing when there's an error in mysql

--- routes/apiRouter.js
+++ routes/apiRouter.js
@@ -28,14 +28,19 @@ router.get('/query1', (req, res) => {
     var genre = '%' + data.genre + '%';
     var limit = parseInt(data.limit);
     limit = (isNaN(limit)) ? 1073741824 : limit; 
+
     conn.query('SELECT title, genre, year FROM movies WHERE genre LIKE ? AND year BETWEEN ? AND ? ORDER BY year LIMIT ?', [genre,parseInt(data.startYear),parseInt(data.endYear), limit], 
-        function (err, results, fields) {
-            if (err) throw err;
-            else console.log('Selected ' + results.length + ' row(s).');
-            
+    function (err, results, fields) {
+        if (err) {
+            console.error(err);
+            res.json([{id:1, error: "Query failed. Check your inputs."}]);
+        }
+        else {
+            console.log('Selected ' + results.length + ' row(s).');
             res.send(results);
-            console.log("Done.");
-        })
+        }
+        console.log("Query function done.");
+    })
 });
 
 router.get('/query2', (req, res) => {
@@ -49,11 +54,12 @@ router.get('/query2', (req, res) => {
     limit = (isNaN(limit)) ? 1073741824 : limit; 
     conn.query('SELECT title, country, language, year FROM movies WHERE language LIKE ? AND country LIKE ? AND year BETWEEN ? AND ? ORDER BY year LIMIT ?', [language,country,parseInt(data.startYear),parseInt(data.endYear), limit], 
         function (err, results, fields) {
-            if (err) throw err;
-            else console.log('Selected ' + results.length + ' row(s).');
-            
-            res.send(results);
-            console.log("Done.");
+            if (err) console.error(err);
+            else {
+                console.log('Selected ' + results.length + ' row(s).');
+                res.send(results);
+                console.log("Done.");
+            }
         })
 });
 
@@ -68,11 +74,12 @@ router.get('/query3', (req, res) => {
     limit = (isNaN(limit)) ? 1073741824 : limit; 
     conn.query('SELECT DISTINCT name, COUNT(names.imdb_name_id) AS "Number of Leading Roles" FROM names JOIN title_principals ON names.imdb_name_id = title_principals.imdb_name_id WHERE title_principals.ordering = 1 AND (title_principals.category = "actress" OR title_principals.category = "actor") GROUP BY names.imdb_name_id HAVING COUNT(*) > ? ORDER BY COUNT(names.imdb_name_id) DESC LIMIT ?',[parseInt(data.n_leadingroles), limit], 
         function (err, results, fields) {
-            if (err) throw err;
-            else console.log('Selected ' + results.length + ' row(s).');
-            
-            res.send(results);
-            console.log("Done.");
+            if (err) console.error(err);
+            else {
+                console.log('Selected ' + results.length + ' row(s).');
+                res.send(results);
+                console.log("Done.");
+            }
         })
 });
 
@@ -86,11 +93,12 @@ router.get('/query4', (req, res) => {
     limit = (isNaN(limit)) ? 1073741824 : limit; 
     conn.query('SELECT DISTINCT m.title, r.weighted_average_vote AS rating FROM movies m CROSS JOIN ratings r ON m.imdb_title_id = r.imdb_title_id WHERE m.year = ? AND r.weighted_average_vote BETWEEN ? AND ? ORDER BY rating DESC LIMIT ?', [parseInt(data.year),parseInt(data.min_avg_vote),parseInt(data.max_avg_vote), limit], 
         function (err, results, fields) {
-            if (err) throw err;
-            else console.log('Selected ' + results.length + ' row(s).');
-            
-            res.send(results);
-            console.log("Done.");
+            if (err) console.error(err);
+            else {
+                console.log('Selected ' + results.length + ' row(s).');
+                res.send(results);
+                console.log("Done.");
+            }
         })
 });
 
@@ -106,11 +114,12 @@ router.get('/query5', (req, res) => {
     limit = (isNaN(limit)) ? 1073741824 : limit; 
     conn.query('SELECT n.name AS "Actor/Actress", COUNT(n.name) AS "Number of Romance Films casted from 2010-2020 in USA", MAX(m.year) AS "Year of Last Romance Movie" FROM movies m INNER JOIN title_principals p ON m.imdb_title_id = p.imdb_title_id AND (p.category = "actor" OR p.category = "actress") INNER JOIN names n ON p.imdb_name_id = n.imdb_name_id WHERE m.year >= ? AND m.year <= ? AND m.country LIKE ? AND m.genre LIKE ? GROUP BY n.name ORDER BY COUNT(n.name) DESC, MAX(m.year) DESC, n.name ASC LIMIT ?', [parseInt(data.startYear), parseInt(data.endYear), country, genre, limit], 
         function (err, results, fields) {
-            if (err) throw err;
-            else console.log('Selected ' + results.length + ' row(s).');
-            
-            res.send(results);
-            console.log("Done.");
+            if (err) console.error(err);
+            else {
+                console.log('Selected ' + results.length + ' row(s).');
+                res.send(results);
+                console.log("Done.");
+            }
         })
 });
 
@@ -125,11 +134,12 @@ router.get('/query6', (req, res) => {
     limit = (isNaN(limit)) ? 1073741824 : limit; 
     conn.query('SELECT n.name AS "Director", n.place_of_birth AS "Place of Birth", COUNT(n.name) AS "Number of Films Directed in the USA", MAX(m.year) AS "Year of Last Film" FROM movies m INNER JOIN title_principals p ON m.imdb_title_id = p.imdb_title_id AND p.category = "director" INNER JOIN names n ON p.imdb_name_id = n.imdb_name_id AND n.place_of_birth != "" AND n.place_of_birth NOT LIKE ? WHERE m.country LIKE ? GROUP BY n.name, n.place_of_birth ORDER BY COUNT(n.name) DESC, MAX(m.year) DESC, n.name ASC LIMIT ?', [country, country, limit],
         function (err, results, fields) {
-            if (err) throw err;
-            else console.log('Selected ' + results.length + ' row(s).');
-            
-            res.send(results);
-            console.log("Done.");
+            if (err) console.error(err);
+            else {
+                console.log('Selected ' + results.length + ' row(s).');
+                res.send(results);
+                console.log("Done.");
+            }
         })
 });
 
@@ -143,11 +153,12 @@ router.get('/query7', (req, res) => {
     limit = (isNaN(limit)) ? 1073741824 : limit; 
     conn.query('SELECT n.name AS "Name", t.category AS "Gender", COUNT(t.imdb_name_id) AS "Number of Roles" FROM names n LEFT JOIN title_principals t ON n.imdb_name_id = t.imdb_name_id LEFT JOIN movies m ON t.imdb_title_id = m.imdb_title_id LEFT JOIN ratings r ON m.imdb_title_id = r.imdb_title_id WHERE (t.category = "actor" OR t.category = "actress") AND (YEAR(NOW()) - YEAR(n.date_of_birth) >= ?) AND (YEAR(NOW()) - YEAR(n.date_of_birth) <= ?) AND m.country = ? AND (r.weighted_average_vote >= ?) AND (r.weighted_average_vote <= ?) GROUP BY t.imdb_name_id, t.category ORDER BY n.name LIMIT ?', [parseInt(data.minAge), parseInt(data.maxAge), data.country, parseInt(data.minRating), parseInt(data.maxRating), limit],
         function (err, results, fields) {
-            if (err) throw err;
-            else console.log('Selected ' + results.length + ' row(s).');
-            
-            res.send(results);
-            console.log("Done.");
+            if (err) console.error(err);
+            else {
+                console.log('Selected ' + results.length + ' row(s).');
+                res.send(results);
+                console.log("Done.");
+            }
         })
 });
 
